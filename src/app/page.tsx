3f22c5cd6bf693c6ca81567e5c3116afb8b4d394
demo_repo <dinmuhammadrajpar@gmail.com>
2024-12-23 02:22:45 +0500

    made some more changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import HeroSection from "./components/hero-section/page";
 import FeaturesSection from "./components/Features-Section/page";
 import Listing from "./components/Listing-Section/page";
